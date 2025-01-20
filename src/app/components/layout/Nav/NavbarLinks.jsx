a58ae767027cc493c244c2216fda41a3c0796b48
Danial Alborz <danialalborz3@gmail.com>
2025-01-20 17:27:56 +0330

    activate Text me btn

--- src/app/components/layout/Nav/NavbarLinks.jsx
+++ src/app/components/layout/Nav/NavbarLinks.jsx
@@ -3,7 +3,7 @@
 const NavbarLinks = () => {
     return (
 
-        <a className="p-2 mx-2 bg-[#8FB4EA] dark:bg-[#304A65] rounded-lg font-bebasNeue text-2xl dark:text-[#829FC7] text-black bg-none hover:bg-[#829FC7] hover:animate-pulse dark:hover:text-black" href="">Text Me!</a>
+        <a className="p-2 mx-2 bg-[#8FB4EA] dark:bg-[#304A65] rounded-lg font-bebasNeue text-2xl dark:text-[#829FC7] text-black bg-none hover:bg-[#829FC7] hover:animate-pulse dark:hover:text-black" href="https://comments-neon-five.vercel.app/" target="_blank">Text Me!</a>
 
     );
 };
